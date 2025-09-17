fbc61e06bd4e3c9f1c3af4b94d09169ad9ff2251
Alessandro Venanzi <alessandro.venanzi1@it.ey.com>
2025-09-17 17:43:24 +0200

    added Lab4

---
+++ Lab4_Git/main.py
@@ -0,0 +1,6 @@
+# utils/__main__.py
+def main():
+    print("Modulo eseguito direttamente")
+
+if __name__ == "__main__":
+    main()
---
+++ Lab4_Git/utils/functions.py
@@ -0,0 +1,8 @@
+def prodotto(a,b):
+    return a * b
+
+def somma(a,b):
+    return a + b
+
+def differenza(a,b):
+    return a - b
